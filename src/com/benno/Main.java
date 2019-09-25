88ffe7d943c428fab576548c24280ad9e5c78fa5
BennoAlif <bennoalif41@gmail.com>
2019-09-25 16:10:39 +0700

    Initial commit

---
+++ src/com/benno/Main.java
@@ -0,0 +1,20 @@
+package com.benno;
+
+    /*
+    NAMA        : BENNO ALIF ANGGARA
+    KELAS       : IF1
+    NIM         : 10118024
+    Deskripsi   : Mendeklarasikan variabel pecahan
+     */
+
+public class Main {
+
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+    }
+}
